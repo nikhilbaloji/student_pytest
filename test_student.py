876e35fc76b00654019133afc54f02e97e11bfd3
nikhilbaloji <nikhilbaloji@gmail.com>
2026-01-07 02:50:49 +0530

    save

---
+++ test_student.py
@@ -0,0 +1,28 @@
+from student import process_student_data
+
+def test_default_values():
+    result = process_student_data(["student.py"])
+
+    assert result["name"] == "Nikhil"
+    assert result["department"] == "BCA"
+    assert result["semester"] == "3"
+    assert result["total"] == 283
+    assert result["grade"] == "A"
+
+def test_command_line_arguments():
+    args = [
+        "student.py",
+        "samarth",
+        "BCA",
+        "3",
+        "70",
+        "75",
+        "80"
+    ]
+
+    result = process_student_data(args)
+
+    assert result["name"] == "samarth"
+    assert result["total"] == 225
+    assert result["average"] == 75
+    assert result["grade"] == "C"
